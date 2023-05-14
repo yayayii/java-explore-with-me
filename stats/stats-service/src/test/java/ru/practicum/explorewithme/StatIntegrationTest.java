924546ebec06fec5be5f22af2addce1c359082c8
yayayi <evgegnii-homichk@yandex.ru>
2023-05-14 16:40:44 +0300

    refactor: added dto module

--- stats/stats-service/src/test/java/ru/practicum/explorewithme/StatIntegrationTest.java
+++ stats/stats-service/src/test/java/ru/practicum/explorewithme/StatIntegrationTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.explorewithme.dto.StatFullResponseDto;
 import ru.practicum.explorewithme.dto.StatRequestDto;
