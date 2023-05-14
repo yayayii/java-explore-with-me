adb06c18305cfc22e5c0ddd5ca225ef7442e6c36
yayayi <evgegnii-homichk@yandex.ru>
2023-05-14 22:56:36 +0300

    feat: created stat client

---
+++ stats/stats-client/src/main/java/ru/practicum/explorewithme/StatClient.java
@@ -0,0 +1,25 @@
+package ru.practicum.explorewithme;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import ru.practicum.explorewithme.dto.StatRequestDto;
+
+@Service
+public class StatClient extends BaseClient {
+    @Autowired
+    public StatClient(@Value("-") String serverUrl, RestTemplateBuilder builder) {
+        super(
+                builder.uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl))
+                        .requestFactory(HttpComponentsClientHttpRequestFactory::new).build()
+        );
+    }
+
+    public ResponseEntity<Object> saveEndpointRequest(StatRequestDto statsRequestDto) {
+        return post("/hit", statsRequestDto);
+    }
+}
