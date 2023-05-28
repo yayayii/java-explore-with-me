7c3b44f86ad61df7571882b21de47f8864d6a855
yayayi <evgegnii-homichk@yandex.ru>
2023-05-28 17:47:55 +0300

    fix:

--- main-service/src/main/java/ru/practicum/explorewithme/dto/event/EventUpdateRequestDto.java
+++ main-service/src/main/java/ru/practicum/explorewithme/dto/event/EventUpdateRequestDto.java
@@ -12,7 +12,6 @@ import ru.practicum.explorewithme.util.Private;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Future;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PositiveOrZero;
 import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
